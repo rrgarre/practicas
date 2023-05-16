f61cc1583606039e28bbb238158568f5463b3cdb
rrgarre <rrgarre@gmail.com>
2023-05-16 18:01:52 +0200

    Boton para mostrar u ocultar LoginForm

--- src/componentes/LoginForm.js
+++ src/componentes/LoginForm.js
@@ -1,28 +1,34 @@
-
+import {useState} from 'react'
 
 const LoginForm = ({handleSubmit, username, password, setPassword, setUsername}) => {
+
+  const [showForm, setShowForm] = useState(false)
+
   return(
-    <form onSubmit={handleSubmit}>
-      <div>
-        Nombre de usuario
-        <input
-          type='text'
-          value={username}
-          onChange={({target})=>{setUsername(target.value)}}
-          name='Username'
-        />
-      </div>
-      <div>
-        Contraseña
-        <input
-          type='password'
-          value={password}
-          onChange={({target})=>{setPassword(target.value)}}
-          name='Password'
-        />
-      </div>
-      <button type='submit'>login</button>
-    </form>
+    !showForm
+      ? <button onClick={()=>{setShowForm(true)}}>log in</button>
+      : <form onSubmit={handleSubmit}>
+          <div>
+            Nombre de usuario
+            <input
+              type='text'
+              value={username}
+              onChange={({target})=>{setUsername(target.value)}}
+              name='Username'
+            />
+          </div>
+          <div>
+            Contraseña
+            <input
+              type='password'
+              value={password}
+              onChange={({target})=>{setPassword(target.value)}}
+              name='Password'
+            />
+          </div>
+          <button type='submit'>login</button>
+          <button onClick={()=>{setShowForm(false)}}>cancel</button>
+        </form>
   )
 }
 
